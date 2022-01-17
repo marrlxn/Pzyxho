2a4050bb9185f13fc9a45d664bca1f1a5ce4c220
marrlxn <92260588+marrlxn@users.noreply.github.com>
2022-01-17 17:19:36 +0100

    Add files via upload

---
+++ Pzyxho/home/about/sound.js
@@ -0,0 +1,11 @@
+let more = document.querySelector('.more');
+
+let audioArr = document.getElementsByTagName('audio');
+console.log(audioArr);
+
+more.addEventListener('mouseenter', () => {
+    audioArr[0].play()
+});
+more.addEventListener('mouseenter', () => {
+    audioArr[0].play()
+});
